321cf36478e0af4b3739fbfa37a950f72faabf49
Mauro Mescia <mauro.i.mescia@gmail.com>
2015-11-08 22:41:07 -0500

    run function test

--- app/js/app.js
+++ app/js/app.js
@@ -4,4 +4,3 @@
 
 var mainApp = angular.module('mainApp', ['mainController']);
 
-
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,8 +1,13 @@
 'use strict';
 
 /* Controllers */
-var mainApp = angular.module('mainApp', []);
+var mainApp = angular.module('mainApp', [])
+    .run(['$rootScope','$window',
+           function($rootScope, $window) {
+        $rootScope.running = "Run method"
+    }]);
+
 
 mainApp.controller('mainController', ['$scope', function($scope) {
   $scope.test = "hello Angular";
-}]);
+}])
